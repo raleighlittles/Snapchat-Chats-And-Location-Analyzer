acbdb3708720fd0a3c84b1a20379a753767f1c93
Raleigh Littles III <raleighlittles@gmail.com>
2024-01-23 21:53:30 -0800

    pep

--- snapchat_chat_parser.py
+++ snapchat_chat_parser.py
@@ -7,7 +7,7 @@ import os
 import argparse
 
 
-def export_messages_to_files(parsed_chat_history : dict, output_dir):
+def export_messages_to_files(parsed_chat_history: dict, output_dir):
 
     num_distinct_contacts = len(parsed_chat_history.keys())
 
@@ -35,7 +35,6 @@ def export_messages_to_files(parsed_chat_history : dict, output_dir):
                     parsed_chat_history[username], output_json_file, indent=4, ensure_ascii=False)
 
 
-
 def parse_chats_from_file(chat_history_filepath, year_created):
 
     with open(file=chat_history_filepath, mode='r', encoding="utf-8") as json_file:
@@ -56,7 +55,6 @@ def parse_chats_from_file(chat_history_filepath, year_created):
 
                 for chat in full_chat_history[chat_type]:
 
-
                     # A chat object looks like:
                     # {'From': '<user>', 'Media Type': 'TEXT', 'Created': '2022-07-22 19:28:21 UTC', 'Text': 'Hello World'}
 
@@ -67,7 +65,8 @@ def parse_chats_from_file(chat_history_filepath, year_created):
                         username = chat["To"]
 
                     else:
-                        raise ValueError(f"Unknown value for chat type: '{chat_type}'")
+                        raise ValueError(
+                            f"Unknown value for chat type: '{chat_type}'")
 
                     # To make sorting easier ...
                     unix_timestamp_for_chat = int(datetime.datetime.strptime(
@@ -83,9 +82,9 @@ def parse_chats_from_file(chat_history_filepath, year_created):
 
                     parsed_chat_history[username].append(chat)
 
-                print(f"Finished analyzing {len(full_chat_history[chat_type])} {chat_type} messages")
+                print(
+                    f"Finished analyzing {len(full_chat_history[chat_type])} {chat_type} messages")
 
-                        
         elif year_created >= 2024:
 
             # The new chat objects are much easier to work with. They consist of a nested dictionary, where the first level of keys are the senders,
@@ -117,17 +116,18 @@ def parse_chats_from_file(chat_history_filepath, year_created):
 
                             if chat["IsSender"] == True:
 
-                                #username = new_chat_obj["To"]
+                                # username = new_chat_obj["To"]
                                 new_chat_obj["To"] = username
 
                             elif chat["IsSender"] == False:
 
-                                #username = chat["From"]
+                                # username = chat["From"]
                                 new_chat_obj["From"] = username
 
                             else:
-                                raise ValueError(f"Unknown value for `IsSender` key, expected only true or false, got: {chat['IsSender']}")
-                            
+                                raise ValueError(
+                                    f"Unknown value for `IsSender` key, expected only true or false, got: {chat['IsSender']}")
+
                             # Set dates
                             new_chat_obj["Created-Timestamp"] = chat["Created(microseconds)"]
                             new_chat_obj["Created"] = chat["Created"]
@@ -136,10 +136,10 @@ def parse_chats_from_file(chat_history_filepath, year_created):
                             if chat["Content"] is not None and len(chat["Content"]) > 0:
                                 new_chat_obj["Text"] = chat["Content"]
 
-                            
                             parsed_chat_history[username].append(new_chat_obj)
 
-                    print(f"Finished analyzing {len(parsed_chat_history[username])} messages from {username}")
+                    print(
+                        f"Finished analyzing {len(parsed_chat_history[username])} messages from {username}")
 
         return parsed_chat_history
 
@@ -148,33 +148,25 @@ if __name__ == "__main__":
 
     argparse_parser = argparse.ArgumentParser()
 
-    argparse_parser.add_argument("-i", "--input-chat-history-json", required=True, type=str, help="The path to the `chat_history.json` file")
+    argparse_parser.add_argument("-i", "--input-chat-history-json", required=True,
+                                 type=str, help="The path to the `chat_history.json` file")
 
-    argparse_parser.add_argument("-y", "--year", required=True, type=int, help="The year the snapchat dump was taken. Needed to determine format.")
+    argparse_parser.add_argument("-y", "--year", required=True, type=int,
+                                 help="The year the snapchat dump was taken. Needed to determine format.")
 
-    argparse_parser.add_argument("-o", "--output-dir", required=True, type=str, help="The output directory to put the JSON files")
+    argparse_parser.add_argument("-o", "--output-dir", required=True,
+                                 type=str, help="The output directory to put the JSON files")
 
     argparse_args = argparse_parser.parse_args()
-
 
     chat_history_filepath = argparse_args.input_chat_history_json
 
     if os.path.exists(chat_history_filepath):
 
-        parsed_chat_history = parse_chats_from_file(chat_history_filepath, argparse_args.year)
+        parsed_chat_history = parse_chats_from_file(
+            chat_history_filepath, argparse_args.year)
         export_messages_to_files(parsed_chat_history, argparse_args.output_dir)
 
     else:
-        raise FileNotFoundError(f"Error couldn't find chat history file: '{chat_history_filepath}'")
-
-        
-
-
-
-
-
-
-
-
-                        
-
+        raise FileNotFoundError(
+            f"Error couldn't find chat history file: '{chat_history_filepath}'")
